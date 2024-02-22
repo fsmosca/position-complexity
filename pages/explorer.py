23cafd2cc95e3847a59253fc6cd1bd6b149c07d6
fsmosca <fsmosca@users.noreply.github.com>
2024-02-23 05:17:37 +0800

    Create explorer.py

---
+++ pages/explorer.py
@@ -0,0 +1,18 @@
+import pygwalker as pyg
+from pygwalker.api.streamlit import init_streamlit_comm
+import streamlit as st
+from modules.pygwalker import get_pyg_renderer
+
+
+init_streamlit_comm()
+
+
+st.sidebar.page_link('app.py', label='Home', icon='🏡')
+st.sidebar.page_link('pages/explorer.py', label='Explorer', icon='🏝️')
+ 
+# Add Title
+st.title("Explorer")
+st.markdown(':blue[**Drag and drop**] fields into the Y and X axes or to filters, colors, etc.')
+
+renderer = get_pyg_renderer()
+renderer.render_explore()
