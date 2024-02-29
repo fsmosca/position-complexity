3b28a1d93ce8c7092cd4f3822810db3a2cbb6e8d
fsmosca <fsmosca@users.noreply.github.com>
2024-02-29 17:34:10 +0800

    Update explorer.py

--- pages/explorer.py
+++ pages/explorer.py
@@ -1,8 +1,8 @@
-import pygwalker as pyg
 from pygwalker.api.streamlit import init_streamlit_comm
 import streamlit as st
 from streamlit import session_state as ss
 from modules.pygwalker import get_pyg_renderer
+from modules.menu import get_menu
 
 
 st.set_page_config(
@@ -18,14 +18,10 @@ if 'selected_pos' not in ss:
 
 init_streamlit_comm()
 
-
-st.sidebar.page_link('app.py', label='Home', icon='🏡')
-st.sidebar.page_link('pages/explorer.py', label='Explorer', icon='🏝️')
-st.sidebar.page_link('pages/positions.py', label='Positions', icon='💫')
-st.sidebar.page_link('pages/generator.py', label='Generator', icon='🌀')
+get_menu()
  
 # Add Title
-st.title("Explorer")
+st.title("🌍 Explorer")
 st.markdown(':blue[**Drag and drop**] fields into the Y and X axes or to filters, colors, etc.')
 
 renderer = get_pyg_renderer()
